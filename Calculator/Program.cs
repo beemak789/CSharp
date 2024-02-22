8481c71909549dfe57cc1d5999a25209d26872e1
Brandy <81196479+beemak789@users.noreply.github.com>
2024-02-22 13:02:55 -0500

    first-commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+
+var firstAsText = Console.ReadLine();
+// what if the user does not input a number?
+var number1 = int.Parse(firstAsText);
+
+Console.WriteLine("Input the second number");
+var secondAsText = Console.ReadLine();
+var number2 = int.Parse(secondAsText);
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+var choice = Console.ReadLine();
+
+    if (choice == "A" || choice == "a")
+    {
+        var sum = number1 + number2;
+        PrintFinalEquation(number1, number2, sum, "+");
+    }
+    else if (choice == "S" || choice == "s")
+    {
+        var difference = number1 - number2;
+        PrintFinalEquation(number1, number2, difference, "-");
+    }
+    else if (choice == "M" || choice == "m")
+    {
+        var product  = number1 * number2;
+        PrintFinalEquation(number1, number2, product, "*");
+    }
+    else
+    {
+        Console.WriteLine("Invalid option");
+    }
+
+
+    //operator is a reserved keyword 
+void PrintFinalEquation (int num1, int num2, int result, string @operator)
+{
+    Console.WriteLine($"{num1} {@operator} {num2} = {result}");
+}
+
+
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
---
+++ Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿// do while loop
+// condition is checked
+// as long as the do condition is met, the while condition will be met
+// the while loop will always be executed after checking the do condition is met
+
+//using System.Net.NetworkInformation;
+
+//Do -while loop - RepeatCharacter
+//Using the do ...while loop, implement the RepeatCharacter method,
+// which given a character and targetLength, should build a string of this character
+// repeated the given number of times.
+
+//The result must contain at least one character, so even if the targetLength is zero,
+//the length of the result should be 1.
